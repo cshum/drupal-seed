42909bbd00485427f76a5710181daa8365fc3821
Adrian C Shum <adrian@cshum.com>
2014-11-15 07:19:14 +0800

    fix

--- nginx.sh
+++ nginx.sh
@@ -4,5 +4,5 @@ if [ "$#" -ne 2 ]; then
   echo "Usage: $0  <nginx conf path> <port>"
 else
   # write config
-  sed -e "s;%ROOT%;`pwd`;g" -e "s;%PORT%;$2;g" scripts/nginx.conf > $1
+  sed -e "s;%ROOT%;`pwd`;g" -e "s;%PORT%;$2;g" nginx.conf > $1
 fi
